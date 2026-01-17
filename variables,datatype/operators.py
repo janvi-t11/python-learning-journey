5dd2bec569c20b340066b393c2fa7688dbdfc33c
janvi-t11 <janvithakre2@gmail.com>
2026-01-18 01:18:50 +0530

    Update Python files

--- variables,datatype/operators.py
+++ variables,datatype/operators.py
@@ -1,5 +1,5 @@
 # Arithmetic Operators in Python
-"""a = 15
+a = 15
 b = 5 
 
 print(a + b) # Addition 20
@@ -8,10 +8,10 @@ print(a * b) # Multiplication 75
 print(a / b) # Division 3.0
 print(a // b) # Floor Division 3
 print(a % b) # Modulo 0
-print (a ** b) # Power 759375"""
+print (a ** b) # Power 759375
 
 #comparison Operators in Python
-"""a = 10 
+a = 10 
 b = 20
 
 print(a == b) # Equal  (Answer:False)
@@ -19,36 +19,36 @@ print(a != b) # Not Equal (Answer:True)
 print(a > b)  # Greater Than (Answer:False)
 print(a < b)  # Less Than (Answer:True)
 print(a>= b) # Greater Than or Equal to (Answer:False)
-print(a <= b) # Less Than or Equal to (Answer:True)"""
+print(a <= b) # Less Than or Equal to (Answer:True)
 
 # Logical Operators in Python
-"""a = 26 
+a = 26 
 b = 10 
 print(a > 15 and b < 15) # True
 print(a > b and b > a) # False
 print(a > b or b  > a) # True
 print(a < b or b < a) # False
 print(not (a == b)) # True
-print(not (a > b)) # False """
+print(not (a > b)) # False 
 
 #truth table of "and" operator:
-"""print(True and True)   # True
+print(True and True)   # True
 print(True and False)  # False
 print(False and True)  # False
-print(False and False) # False"""
+print(False and False) # False
 
 #truth table of "or" operator:
-"""print(True or True)   # True
+print(True or True)   # True
 print(True or False)  # True
 print(False or True)  # True
-print(False or False) # False"""
+print(False or False) # False
 
 #truth table of "not" operator:
-"""print(not True)  # False
-print(not False) # True"""
+print(not True)  # False
+print(not False) # True
 
 # Assignment Operators in Python
-"""a = 10 # Assign 10 to a (Answer:10)
+a = 10 # Assign 10 to a (Answer:10)
 print(a)
 a +=5 # a = a + 5 (Answer:15)
 print(a)
@@ -63,5 +63,5 @@ print(a)
 a **=5 # a = a ** 5 (Answer:7.59375)
 print (a)
 a //=4 # a = a // 4 (Answer:1.0)
-print(a)"""
+print(a)
 
--- variables,datatype/practise set.py
+++ variables,datatype/practise set.py
@@ -1,29 +1,29 @@
 #Write a python program to add two numbers.
-"""a = 5
+a = 5
 b = 6 
-print(a +b)"""
+print(a +b)
 
 # Write a python program to find remainder when a number is divided by z.
-"""Y = 5
+Y = 5
 Z = 2
-print(Y % Z)"""
+print(Y % Z)
 
 # Check the type of variable assigned using input () function.
-"""A = input("10")
-print(type(A))"""
+A = input("10")
+print(type(A))
 
 """ Use comparison operator to find out whether ‘a’ given variable a is greater than 
 ‘b’ or not. Take a = 34 and b = 80 """
-"""a = 34
+a = 34
 b = 84
 
-print(a > b)"""
+print(a > b)
 
 #Write a python program to find an average of two numbers entered by the user.
-"""num1 = int(input( "enter your number1:"))
+num1 = int(input( "enter your number1:"))
 num2 = int(input("enter your number2:"))
-print((num1 + num2)/2)"""
+print((num1 + num2)/2)
 
 #Write a python program to calculate the square of a number entered by the user.
-"""num = int(input("enter number:"))
-print(num*num)"""
+num = int(input("enter number:"))
+print(num*num)
