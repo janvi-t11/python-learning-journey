7c0b6f7496c7a9ed7db3d0ad5b8628a1348c65c6
janvi-t11 <janvithakre2@gmail.com>
2026-01-19 23:26:01 +0530

    Practised string and operators problems

---
+++ Strings/practise set.py
@@ -0,0 +1,11 @@
+# Write a python program to display a user entered name followed by Good Afternoon using input () function. 
+"""name = str(input("enter your name:"))
+print("Good Afternoon", name)"""
+
+#Write a program to detect double space in a string. 
+"""string = str(input("enter a string:"))
+if " " in string:
+    print("double space found") """#--- IGNORE --- hello  my  name is johnn 
+
+
+
--- Strings/slicing.py
+++ Strings/slicing.py
@@ -6,3 +6,10 @@ print(a[:5]) #Hello
 print(a[2:]) #llo, john
 print(a[:]) #Hello, john
 print(a[-9:-3]) #lo, j
+
+#slice with skipping
+b = "abcdefghijklmnopqrstuvwxyz"
+print(b[1:8:3]) #beh
+print(b[0:9:5]) #af
+print(b[5:20:5]) #fjou
+print(b[::4]) #aeimquy
